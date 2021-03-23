ea5e716acf2e17470fa768918892e3ba967fe525
lucasA27 <44438409+lucasA27@users.noreply.github.com>
2021-03-23 12:11:38 -0300

    Update lcd.c

--- Projeto1-ControleTemperatura/lcd/lcd.c
+++ Projeto1-ControleTemperatura/lcd/lcd.c
@@ -32,63 +32,6 @@ void typeln(const char *s);
 void typeChar(char val);
 int fd;  // seen by all subroutines
 
-/*int main()   {
-
-  if (wiringPiSetup () == -1) exit (1);
-
-  fd = wiringPiI2CSetup(I2C_ADDR);
-
-  //printf("fd = %d ", fd);
-
-  lcd_init(); // setup LCD
-
-  char array1[] = "Hello world!";
-
-  while (1)   {
-
-    lcdLoc(LINE1);
-    typeln("Using wiringPi");
-    lcdLoc(LINE2);
-    typeln("Geany editor.");
-
-    delay(2000);
-    ClrLcd();
-    lcdLoc(LINE1);
-    typeln("I2c  Programmed");
-    lcdLoc(LINE2);
-    typeln("in C not Python.");
-
-    delay(2000);
-    ClrLcd();
-    lcdLoc(LINE1);
-    typeln("Arduino like");
-    lcdLoc(LINE2);
-    typeln("fast and easy.");
-
-    delay(2000);
-    ClrLcd();
-    lcdLoc(LINE1);
-    typeln(array1);
-
-    delay(2000);
-    ClrLcd(); // defaults LINE1
-    typeln("Int  ");
-    int value = 20125;
-    typeInt(value);
-
-    delay(2000);
-    lcdLoc(LINE2);
-    typeln("Float ");
-    float FloatVal = 10045.25989;
-    typeFloat(FloatVal);
-    delay(2000);
-  }
-
-  return 0;
-
-}*/
-
-
 // float to string
 void typeFloat(float myFloat)   {
   char buffer[20];
